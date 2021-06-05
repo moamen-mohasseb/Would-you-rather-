1fb7e9afaee8c9aed0c81758383537798a712430
moamen mohasseb <moamen_mohasseb@yahoo.com>
2021-06-05 21:15:17 +0200

    Update index to accept redux store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,12 @@
 import { combineReducers } from 'redux'
 import authedUser from './userAuth'
 import users from './users'
-import question from './questions'
+import questions from './questions'
 import { loadingBarReducer } from 'react-redux-loading'
 
 export default combineReducers({
   authedUser,
   users,
-  question,
+  questions,
   loadingBar: loadingBarReducer,
 })
