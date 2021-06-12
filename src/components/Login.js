4d2d56faa67a8c4de6f3772594dc94ff59c30e44
moamen mohasseb <moamen_mohasseb@yahoo.com>
2021-06-12 21:15:18 +0200

    Finishing Dashboard QuestionView and Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import LoadingBar from 'react-redux-loading'
 class  Login extends Component {
+  handelSubmit = (e, id) => {
+    e.preventDefault()
+    this.props.history.push(`/results/${id}`)
+  }
   
   render(){
       console.log("users:",this.props.userid)
@@ -11,17 +15,21 @@ class  Login extends Component {
     <div className="App">
    Login
    <h3>Welcome to</h3>
+
    <form>
    <h4>Would you Rather</h4>
    <select>
    <LoadingBar />
    {userid.map(user =>(
-       <option value={user[0]}  key={user[0]} styles={`background-image : url(${user[1].avatarURL})`}>
+       <option value={user[0]}  key={user[0]} title="https://tylermcginnis.com/would-you-rather/sarah.jpg" >
+       
       {user[1].name}
        </option>
+     
    ) )}
          
    </select>
+   
    <button type='submit' >Sign in</button>
    </form>
     </div>
--- src/components/QuestionView.js
+++ src/components/QuestionView.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
+import {Link,withRouter} from 'react-router-dom'
 class  QuestionView extends Component {
-  
+  toParent = (e, id) => {
+    e.preventDefault()
+    this.props.history.push(`/results/${id}`)
+  }
   render(){
     console.log(this.props)
     
@@ -10,6 +13,7 @@ class  QuestionView extends Component {
     <div className="App">
     {
       <form >
+        <Link className="Poll Button" to={`/results/${this.props.questionData.id  }`} value={this.props.questionData.id} >
         <div><h2>{this.props.user.name} Ask this Question </h2></div>
         <div styles={"background-image : url(./images/1.png)"}>
         <h2>Would you Rather </h2>
@@ -17,7 +21,7 @@ class  QuestionView extends Component {
         <h3>Or</h3>
         <h3>2-{this.props.questionData.optionTwo.text}?</h3>
         </div  >
-        <Link className="Poll Button" to="/results" value={this.props.questionData.id} >view poll</Link>
+        </Link>
 
       </form>
     }
@@ -35,4 +39,4 @@ function mapStateToProps({ questions , users },{id})
         user:UserData
     }
 }
-export default connect(mapStateToProps)(QuestionView);
+export default withRouter(connect(mapStateToProps)(QuestionView))
